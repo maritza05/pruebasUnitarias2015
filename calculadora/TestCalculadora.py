c67b835291d2c370a318fe671e93fdf60e142314
Maritza <maritzaesparza05@gmail.com>
2015-09-11 11:24:52 -0500

    Cambios a la prueba de calculadora

--- calculadora/TestCalculadora.py
+++ calculadora/TestCalculadora.py
@@ -2,9 +2,27 @@ import unittest
 
 class TestCalculadora(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculadora()
+
     def test_sumar_de_2_mas_2(self):
-        calc = Calculadora()
-
-        resultado = calc.suma(2,2)
-
+        resultado = self.calc.suma(2,2)
         self.assertEqual(4, resultado)
+
+    def test_sumar_de_3_mas_3(self):
+        resultado = self.calc.suma(3,3)
+        self.assertEqual(6, resultado)
+
+    def test_sumar_de_5_mas_5(self):
+        resultado = self.calc.suma(5,5)
+        self.assertEqual(10, resultado)
+
+
+class Calculadora():
+    def suma(self, num1, num2):
+        return num1+num2
+
+
+if __name__=="__main__":
+    unittest.main()
+
